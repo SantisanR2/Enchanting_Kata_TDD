864ba7a5d0359f55d6b8b5b1fbdaf8e1dc39a3ea
Santiago Andres Ramirez Ramirez <santiago.ramirez@sinco.com.co>
2025-11-05 16:36:21 -0500

    RED, Cuando encanto con fuego, el arma debe tener la propiedades adicionales del fuego

--- EnchantingTest.cs
+++ EnchantingTest.cs
@@ -1,9 +1,43 @@
-﻿namespace Enchanting;
+﻿using AwesomeAssertions;
+
+namespace Enchanting;
 
 public class EnchantingTest
 {
     [Fact]
-    public void Test1()
+    public void WhenFireEnchant_PrefixInfernoAnd5FireDamage()
     {
+        var baseWeapon = new BaseWeapon();
+        var durance = new Durance(baseWeapon);
+        
+        durance.Enchant("fire");
+
+        durance.DescribeWeapon().Should().Be("Inferno Dagger of the Nooblet\n5 - 10 attack damage\n1.2 attack speed\n+5 fire damage");
+    }
+}
+
+public class Durance(IWeapon weapon)
+{
+    public void Enchant(string enchantment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string DescribeWeapon()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IWeapon
+{
+    string Stats();
+}
+
+public class BaseWeapon: IWeapon
+{
+    public string Stats()
+    {
+        throw new NotImplementedException();
     }
 }
